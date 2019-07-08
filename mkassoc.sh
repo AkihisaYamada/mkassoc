b3cc88959b6b67143b4e340650b51ddfa34b22fc
Akihisa Yamada <ayamada@Akihisas-MacBook-Pro.local>
2019-07-08 19:13:17 +0900

    fixed typo

--- mkassoc.sh
+++ mkassoc.sh
@@ -8,7 +8,7 @@ for a in `seq 1 $n`; do
 for b in `seq 1 $n`; do
 for c in `seq 1 $n`; do
 for d in `seq 1 $n`; do
-echo "m_${a}_${c}_${d}(m_${a}_${b}_${c}(x,y),z) = m_${a}_${b}_${d}(x,m_${b}_${c}_${d}(y,z))"
+echo "m_${a}_${c}_${d}(m_${a}_${b}_${c}(x,y),z) -> m_${a}_${b}_${d}(x,m_${b}_${c}_${d}(y,z))"
 done
 done
 done
